be37f9ba0c24cbed1acb09080ada22d4000e1414
sagarr <sagar.rohankar@gmail.com>
2022-12-06 21:44:43 +0530

    day 6

---
+++ lib/day6/day6.ex
@@ -0,0 +1,19 @@
+defmodule AOC22.Day6 do
+  def solution(input, chunk_size) do
+    input
+    |> String.split("", trim: true)
+    |> Enum.chunk_every(chunk_size, 1, :discard)
+    |> Enum.map(fn chunk -> Enum.map(chunk, fn x -> {x, x} end) end)
+    |> Enum.reduce_while(0, fn chunk, count ->
+      if map_size(Map.new(chunk)) == chunk_size do
+        {:halt, count + chunk_size}
+      else
+        {:cont, count + 1}
+      end
+    end)
+    |> IO.inspect()
+  end
+end
+
+File.read!('lib/day6/input.txt') |> AOC22.Day6.solution(4)
+File.read!('lib/day6/input.txt') |> AOC22.Day6.solution(14)
